9493613fbd9d5481a0b40e5520782db3082df35e
I-Husillos <ivanhuslabra@gmail.com>
2026-05-14 09:31:19 +0200

    creamos el segundo test de comprobar la existencia del controlador de los contactos

--- tests/Feature/AgendaTest.php
+++ tests/Feature/AgendaTest.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Models\Cita;
+use App\Controllers\Http\CitaController;
+
+
+
 // comprobar la existencia de los modelos de citas
 test('Existen los modelos de citas', function () {
-    $this->assertTrue(class_exists(App\Models\Cita::class));
+    $this->assertTrue(class_exists(Cita::class));
 });
 
+// Comprobar la existencia del controlador de los contactos
+test('Existen los controladores de citas', function () {
+    $this->assertTrue(class_exists(CitaController::class));
+});
